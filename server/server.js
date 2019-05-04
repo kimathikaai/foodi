2c0df01b0c343bc4e90b39533018f7302519619f
kimathikaai <kiprotich.kaai@gmail.com>
2019-05-04 18:19:38 -0400

    Determining endpoints to implement

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
 // Implementation of POST, GET, DELETE and PATCH endpoints
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Restuarant
+// GET - Restaurant List
+// POST - Create Restaurant
+// DELETE - Restaurant
+
+// Restuarant Menu
+// GET - Restaurant Menu
+// POST - Create Restaurant Menu
+// PATCH - Update Menu Items
+// DELETE - Menu Item
+
+// User
+// POST - Login
+// POST - Logout
+
