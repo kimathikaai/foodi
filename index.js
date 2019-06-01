c92e9d1d294475c2ddeee9c9cbff82027819428a
nathanrowe4 <nathanrowe@bell.net>
2019-06-01 19:04:46 -0400

    Add user and restaurant routers to the express server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+//Run file to connect to database
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const restaurantRouter = require('./routers/restaurant')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(restaurantRouter)
+
+app.listen(port, () => {
+  console.log('Server is up on port ', port)
+})
