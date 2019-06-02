2364ec0a1ccbbde71be6c6713c0bbb81793fc625
nathanrowe4 <nathanrowe@bell.net>
2019-06-02 15:35:59 -0400

    Comment lines containing restaurant router, as it is currently incomplete

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 //Run file to connect to database
 require('./db/mongoose')
 const userRouter = require('./routers/user')
-const restaurantRouter = require('./routers/restaurant')
+//const restaurantRouter = require('./routers/restaurant')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
-app.use(restaurantRouter)
+//app.use(restaurantRouter)
 
 app.listen(port, () => {
   console.log('Server is up on port ', port)
