fcc911ce811a209359d209d1b3acefcf0a8e0ed7
nathanrowe4 <nathanrowe@bell.net>
2019-06-11 19:27:50 -0400

    Add restaurant router and post request

---
+++ src/routers/restaurant.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const Restaurant = require('../models/restaurant')
+const router = new express.Router()
+
+router.post('/restaurants', async (req, res) => {
+  const restaurant = new Restaurant(req.body)
+
+  try {
+    await restaurant.save()
+    res.status(201).send(restaurant)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
